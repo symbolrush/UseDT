680a46d3c9cabbf5fe99cd5c4cd27e65dbac07c0
Christoph Strauss <christoph.strauss@fhsg.ch>
2016-04-26 09:44:31 +0200

    Minimal example. Only working in Console.

--- R/function.R
+++ R/function.R
@@ -16,12 +16,9 @@ generate_routes_dt <- function(nmax) {
   tmp <- sample(seq(0, 1e7, length.out = 2*nmax), nmax)
   routes$time <- as.integer(tmp)
   routes <- unique(routes)
-  browser()
   routes <- routes[order(lat1, lng1, lat2, lng2, time)]
   data.table::setattr(routes, "sorted", c("lat1", "lng1", "lat2", "lng2"))
   data.table::setkey(routes, lat1, lng1, lat2, lng2)
   search_route <- routes[sample(seq_len(nrow(routes)))[1], ]
-  message(paste0("search in generated data.table for ",
-    paste(search_route, collapse = ", ")))
   return(list(routes = routes, search_route = search_route))
 }
